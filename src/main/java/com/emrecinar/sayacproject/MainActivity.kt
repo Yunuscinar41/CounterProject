c57d5013ff46c01c420e8bffa4dcd0467a91e71e
Yunus Emre Çınar <66219795+Yunuscinar41@users.noreply.github.com>
2021-10-30 23:38:41 +0300

    Add files via upload

---
+++ src/main/java/com/emrecinar/sayacproject/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.emrecinar.sayacproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //abstract class
+
+        object : CountDownTimer(15000,1000) {
+            override fun onTick(p0: Long) {
+                textView.text = "Kalan : ${p0 / 1000}"
+            }
+
+            override fun onFinish() {
+                textView.text = "Kalan : 0"
+            }
+
+        }.start()
+    }
+}
